ede549369826c47179b2757ba74a3b82664351ed
glenchiridza <glenchiridz@gmail.com>
2023-05-18 00:13:47 +0200

    store root words

--- training.py
+++ training.py
@@ -22,3 +22,14 @@ for intent in intents['intents']:
     for pattern in intent['patterns']:
         # seperate words from patterns
         word_list = nltk.word_tokenize(pattern)
+        # add them to words list
+        words.extend(word_list)
+        # associate patterns with respective tags
+        documents.append((word_list, intent['tag']))
+        # append the tags to the class list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# store the root words
+words = [lemmatizer.lemmatize(word)
+         for word in words if word not in ignore_letters]
