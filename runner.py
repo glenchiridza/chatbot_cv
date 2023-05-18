ede82bfdefe8c1839b52a7e7683397ecf329219f
glenchiridza <glenchiridz@gmail.com>
2023-05-18 11:33:34 +0200

    lematize sentences

--- runner.py
+++ runner.py
@@ -6,3 +6,18 @@ import nltk
 from keras.models import load_model
 from nltk.stem import WordNetLemmatizer
 
+lemmatizer = WordNetLemmatizer()
+
+# initializing classes and file contents
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_glen.model')
+
+
+# seperates words from sentences given as input
+def seperate_sentences(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word)
+                      for word in sentence_words]
+    return sentence_words
