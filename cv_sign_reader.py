28b5880244c4342498707791a69630a17e5d2b0f
glenchiridza <glenchiridz@gmail.com>
2023-05-18 21:54:40 +0200

    Finger tips

--- cv_sign_reader.py
+++ cv_sign_reader.py
@@ -1,11 +1,22 @@
 import cv2
 import mediapipe as mp
 import time
+import os
 
+import hand_tracking_cv.hand_tracking_module as htm
+
+wCam, hCam = 640, 480
 video = cv2.VideoCapture(0)
+video.set(3, wCam)
+video.set(4, hCam)
 pTime = 0
+
+detector = htm.HandDetector(min_detect_conf=0.75)
+
+tips_ids = [4, 8, 12, 16, 20]
+
 while True:
-    success,img = video.read()
+    success, img = video.read()
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
@@ -15,4 +26,3 @@ while True:
 
     cv2.imshow("glen_cam", img)
     cv2.waitKey(1)
-
