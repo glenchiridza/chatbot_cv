a10a8ff6cbd4728c01ccb43fb48c6af503b1945c
glenchiridza <glenchiridz@gmail.com>
2023-05-18 11:41:57 +0200

    bag of words lemmatized

--- runner.py
+++ runner.py
@@ -16,8 +16,22 @@ model = load_model('chatbot_glen.model')
 
 
 # seperates words from sentences given as input
-def seperate_sentences(sentence):
+def separate_sentences(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word)
                       for word in sentence_words]
     return sentence_words
+
+
+# bag of words, append 1 to list if the word contained inside input given
+
+def bag_of_words(sentence):
+    sentence_words = separate_sentences(sentence)
+    bag = [0] * len(words)
+    for input_word in sentence_words:
+        for i, word in enumerate(words):
+            # check existence of word in input
+            if word == input_word:
+                # if present assign 1 to the bag within position i--index of the word found
+                bag[i] = 1
+    return np.array(bag)
