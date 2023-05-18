824383919fce9543c6fd8a4844a43aa01353bc93
glenchiridza <glenchiridz@gmail.com>
2023-05-18 10:58:47 +0200

    training

--- training.py
+++ training.py
@@ -1,6 +1,6 @@
-import _random
+import random
 import json
-import _pickle
+import pickle
 import numpy as np
 import nltk
 
@@ -33,3 +33,30 @@ for intent in intents['intents']:
 # store the root words
 words = [lemmatizer.lemmatize(word)
          for word in words if word not in ignore_letters]
+
+words = sorted(set(words))
+
+# saving the words and classes list to binary files ( Serialization )
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(
+        word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+
